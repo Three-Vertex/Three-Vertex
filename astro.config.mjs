d4c9192fae3ed5de84b994c10573d6b803d4e24f
Mohammad Anas <anas.1633.m@gmail.com>
2025-11-23 11:28:35 +0530

    adding custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://three-vertex.github.io',
-    base: '/index',
+    site: 'https://threevertex.co',
 });
